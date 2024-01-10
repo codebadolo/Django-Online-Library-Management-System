aa73e4192f8cc8576060cea0bf1ce819476100e7
codewithbadolo <codewithbadolo@gmail.com>
2024-01-10 14:52:56 +0000

    err

--- in/bin/django-admin.py
+++ in/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/humbu/Django-Online-Library-Management-System/in/bin/python3
+#!/home/codewithbadolo/Django-Online-Library-Management-System/in/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
